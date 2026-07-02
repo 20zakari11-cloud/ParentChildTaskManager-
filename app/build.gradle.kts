e519719515cdc7c7579e8be70c0c211249985427
20zakari11-cloud <20zakari11@gmail.com>
2026-07-02 15:45:06 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,4 +65,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+
+implementation("androidx.activity:activity-ktx:1.9.3")
+implementation("androidx.fragment:fragment-ktx:1.8.5")
+implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.3")
 }
